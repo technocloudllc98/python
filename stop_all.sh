b5eb8fe7d981943608d888b002baa6f796dce9a1
vikram@192.188.100.71 <vikram@192.188.100.71>
2020-06-16 11:13:23 +0530

    added sh

---
+++ stop_all.sh
@@ -0,0 +1,28 @@
+
+#!/bin/bash
+
+for service in  CentralApp Deribit_Snap InMemoryData MessageProce
+do
+
+if pgrep $service >/dev/null 2>&1
+    then
+
+                echo -e "\n"
+                        echo Want to close $service.
+                        echo Yes [Y] or No [N]
+                        read ans
+                                if [ "$ans" == "y" ]
+                                        then
+                                        kill -9 $process_id1
+                                        echo $service stopped.....
+                                        echo -e "\n"
+                                else
+                                        echo $service will be continued..
+                                fi
+    else
+                        echo $service is not running.....
+fi
+
+done
+
+exit 0
